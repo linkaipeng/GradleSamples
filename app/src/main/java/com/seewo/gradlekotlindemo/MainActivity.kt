15d847d4f6763aea2c9f3fbe45de4c8995ce3e91
linkaipeng <linkaipeng1992@gmail.com>
2019-08-27 18:54:44 +0800

    移除无用代码

--- app/src/main/java/com/seewo/gradlekotlindemo/MainActivity.kt
+++ app/src/main/java/com/seewo/gradlekotlindemo/MainActivity.kt
@@ -3,7 +3,6 @@ package com.seewo.gradlekotlindemo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.seewo.annotation.CostTime
-import com.seewo.gradlekotlindemo.dsl.UseDslDemo
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,16 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         test("")
-        UseDslDemo().test()
     }
 
     @CostTime
     fun test(testStr: String) {
-        Thread.sleep(1000)
-    }
-
-    fun testCostTime() {
-        Thread.sleep(1000)
+        Thread.sleep(2000)
     }
 
 }
